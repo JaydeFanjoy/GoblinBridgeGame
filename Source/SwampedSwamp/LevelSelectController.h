dfaf0f851a6f25d2dda2025dc723654c5ba0001f
JaydeFanjoy <jayde.fanjoy@redspace.com>
2016-03-17 09:30:15 -0300

    Added every file needed for the game Jam.

---
+++ Source/SwampedSwamp/LevelSelectController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "LevelSelectController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SWAMPEDSWAMP_API ALevelSelectController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
